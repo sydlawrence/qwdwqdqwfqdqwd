75d7b9d1a27c0664510edb0a05b750d2d306b1c1
Syd Lawrence <sydlawrence@gmail.com>
2011-08-09 17:40:23 +0100

    Ready for external testing

--- check.php
+++ check.php
@@ -36,7 +36,7 @@ $api->setFBUser($_SESSION['fb_user']->id,$session_id,$_SESSION['fb_access_token'
 //the list of private-namespace UIDs to train and search for
 $uids = namespaces();
 
-$threshold = 70;
+$threshold = 10;
 
 
 //search in untagged photos for these uids (can be called multiple times)
