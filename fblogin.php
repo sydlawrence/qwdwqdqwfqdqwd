ae9569867ea4a66f83588da526f0930fd89cc37e
Syd Lawrence <sydlawrence@gmail.com>
2011-08-09 15:25:28 +0100

    got fb login working

--- fblogin.php
+++ fblogin.php
@@ -4,21 +4,16 @@ include('facebook.php');
 
 $fb = new Facebook();
 
-
-
-
-
 if (isset($_GET['code'])) {
   $token = $fb->getAccessToken();
-  $user = $fb->getUser();  
   
-  echo "<pre>User: ".print_r($user,true)."</pre>";
+  $url = "https://graph.facebook.com/me?access_token=".$token;
+
+  $data = json_decode(file_get_contents($url));  
   
-  
-  echo "<pre>Token: ".print_r($token,true)."</pre>";
+  echo "<pre>User: ".print_r($data,true)."</pre>";
 }
 else {
-
 
   header('Location: '.$fb->getLoginUrl());
 }
